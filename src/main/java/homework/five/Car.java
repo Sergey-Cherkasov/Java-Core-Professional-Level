db136f3d9a52ec1e856978f17101aa7971959d8e
Sergey-Cherkasov <s.cherkasov_tester@yahoo.com>
2020-05-19 16:35:52 +0700

    Homework 5. Homework completed.

---
+++ src/main/java/homework/five/Car.java
@@ -0,0 +1,78 @@
+package homework.five;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Car implements Runnable {
+
+    private static int CARS_COUNT;
+    private final Race race;
+    private final int speed;
+    private final CyclicBarrier cb;
+    private final Semaphore semaphore;
+    private final String name;
+
+    static AtomicInteger win_count = new AtomicInteger();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public Car(Race race, int speed, CyclicBarrier cb, Semaphore semaphore) {
+        this.race = race;
+        this.speed = speed;
+        this.cb = cb;
+        this.semaphore = semaphore;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            cb.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < race.getStages().size(); i++) {
+            if (race.getStages().get(i) instanceof Tunnel) {
+                try {
+                    semaphore.acquire();
+                    race.getStages().get(i).go(this);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                race.getStages().get(i).go(this);
+            }
+        }
+
+        if (win_count.incrementAndGet() == 1) {
+            System.out.println(this.name + " - WIN");
+        }
+
+        try {
+            cb.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/homework/five/Stage.java
@@ -0,0 +1,10 @@
+package homework.five;
+
+public abstract class Stage {
+
+    protected int length;
+    protected String description;
+
+    public abstract void go(Car c);
+
+}
